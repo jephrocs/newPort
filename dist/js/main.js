7a932dd6941ef72d3cc71612b79ac697e4812143
Jephrocs <jpconwi@gmail.com>
2020-05-07 13:56:37 -0700

    added picture, about and contact

---
+++ dist/js/main.js
@@ -0,0 +1,44 @@
+// select dom items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+
+// set initial state of menu
+
+let showMenu = false;
+
+
+toggleMenu = () => {
+    if (!showMenu) {
+        console.log("hello")
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //set menu state
+        showMenu = true;
+    } else {
+        console.log("goodbye")
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //set menu state
+        showMenu = false;
+
+    }
+}
+
+chd = () => {
+    console.log("hello i am alive")
+}
+
+menuBtn.addEventListener('click', toggleMenu);
